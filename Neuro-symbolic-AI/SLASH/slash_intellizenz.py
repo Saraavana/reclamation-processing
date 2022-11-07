9f1294e567ed30da6e8e291a1ed9aa4cb5f90e02
Saravana <sgopalakrish@it4ipm.de>
2022-11-07 10:43:59 +0100

    SLASH NN, NN functions defined in train.py

--- Neuro-symbolic-AI/SLASH/slash_intellizenz.py
+++ Neuro-symbolic-AI/SLASH/slash_intellizenz.py
@@ -4,28 +4,26 @@ import train
 
 date_string = datetime.datetime.today().strftime('%d-%m-%Y')
 
-experiments = {'intellizenz_bt_20_5_bs_512_s_20_k_20': 
-                   {'structure': 'binary-trees', 'num_repetitions': 20, 'depth': 5,
-                    'num_sums':20, 'k':20,
-                    'lr': 0.01, 'bs':4096, 'epochs':200, 'use_em':False,
-                    'start_date':date_string, 'resume':True, 'p_num':6, 'credentials':'DO',
+experiments = {'slash-with-nn_intellizenz_bt_20_5_bs_512_s_20_k_20': 
+                   {'lr': 0.01, 'bs':4096, 'epochs':200, 'use_em':False,
+                    'start_date':date_string, 'resume':True, 'p_num':6, 'credentials':'SNN',
               'explanation': """Intellizenz experiment using WMC for a simple classification."""}}
 
-experiments = {'slash-with-nn': 
-                   {'lr': 0.001, 'bs':64, 'epochs':200, 'use_em':False,
-                    'start_date':date_string, 'resume':True, 'p_num':6, 'credentials':'SNN'}}
+# experiments = {'slash-with-nn': 
+#                    {'lr': 0.001, 'bs':64, 'epochs':200, 'use_em':False,
+#                     'start_date':date_string, 'resume':True, 'p_num':6, 'credentials':'SNN'}}
 
-experiments = {'simple-nn': 
-                   {'lr': 0.001, 'bs':64, 'epochs':200, 'use_em':False,
-                    'start_date':date_string, 'resume':True, 'p_num':6, 'credentials':'NN'}}
+# experiments = {'simple-nn': 
+#                    {'lr': 0.001, 'bs':64, 'epochs':200, 'use_em':False,
+#                     'start_date':date_string, 'resume':True, 'p_num':6, 'credentials':'NN'}}
 
-experiments = {'tabnet': 
-                   {'lr': 0.001, 'bs':64, 'epochs':200, 'use_em':False,
-                    'start_date':date_string, 'resume':True, 'p_num':6, 'credentials':'TN'}}
+# experiments = {'tabnet': 
+#                    {'lr': 0.001, 'bs':64, 'epochs':200, 'use_em':False,
+#                     'start_date':date_string, 'resume':True, 'p_num':6, 'credentials':'TN'}}
 
-experiments = {'slash-with-tabnet': 
-                   {'lr': 0.001, 'bs':64, 'epochs':200, 'use_em':False,
-                    'start_date':date_string, 'resume':True, 'p_num':6, 'credentials':'STN'}}
+# experiments = {'slash-with-tabnet': 
+#                    {'lr': 0.001, 'bs':64, 'epochs':200, 'use_em':False,
+#                     'start_date':date_string, 'resume':True, 'p_num':6, 'credentials':'STN'}}
 
 
 
